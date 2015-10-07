f2c3bff2a89dc6a79febe1b9a514a52ef174384d
Ross Bryan <bryanro@gvsu.e.edu>
2015-10-07 17:59:54 -0400

    Added maps/location

--- Capstone/HomeViewController.swift
+++ Capstone/HomeViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, CLLocationManagerDelegate {
+    @IBOutlet weak var MapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        print("\(locationManager.location?.coordinate.latitude)")
+        print("\(locationManager.location?.coordinate.longitude)")
     }
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
     }
+    
+//    func displayLocationInfo(placemark: CLPlacemark) {
+//        if placemark != nil {
+//            //stop updating location to save battery life
+//            locationManager.stopUpdatingLocation()
+//            println(placemark.locality ? placemark.locality : "")
+//            println(placemark.postalCode ? placemark.postalCode : "")
+//            println(placemark.administrativeArea ? placemark.administrativeArea : "")
+//            println(placemark.country ? placemark.country : "")
+//        }
+//    }
 }
---
+++ Capstone/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Capstone
+//
+//  Created by Ross Bryan on 10/7/15.
+//  Copyright © 2015 John Tunisi. All rights reserved.
+//
+
+import Foundation
