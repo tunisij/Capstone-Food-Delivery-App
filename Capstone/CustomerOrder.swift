312cbbe134c13b62014544bb8ccdc3de5f8ccbd6
Ross Bryan <bryanro@gvsu.e.edu>
2015-10-22 15:58:26 -0400

    created an order view controller that the customer will actually fill out the order form with also created the CustomerOrder.swift file that will server as the actual order object to be displayed in the views and stored onto parse

---
+++ Capstone/CustomerOrder.swift
@@ -0,0 +1,29 @@
+//
+//  CustomerOrder.swift
+//  Capstone
+//
+//  Created by Ross Bryan on 10/22/15.
+//  Copyright © 2015 John Tunisi. All rights reserved.
+//
+
+import Foundation
+
+
+class CustomerOrder{
+    let orderName: String;
+    let orderNumber: Int;
+    var orderMessage: String;
+    
+    
+    
+    
+    init(name: String, number: Int, message: String) {
+        orderName = name;
+        orderNumber = number;
+        orderMessage = message;
+    }
+    
+    
+    
+    
+}
