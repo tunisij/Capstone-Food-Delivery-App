6172322f1a97d9fe1ee57170fa73b919c44badc8
Ross Bryan <bryanro@gvsu.e.edu>
2015-11-01 08:21:34 -0500

    some more changes... orders table mostly working now detail view is loading the data

---
+++ Capstone/OrdersTableCell.swift
@@ -0,0 +1,15 @@
+//
+//  OrdersTableCell.swift
+//  Capstone
+//
+//  Created by Ross Bryan on 11/1/15.
+//  Copyright © 2015 John Tunisi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import Parse
+
+class OrdersTableCell: UITableViewCell {
+
+}
--- Capstone/OrdersTableViewController.swift
+++ Capstone/OrdersTableViewController.swift
@@ -18,6 +18,10 @@ class OrdersTableViewController: UITableViewController {
     var nextOrder: CustomerOrder = CustomerOrder(name: "TableTestHeader", number: -1, message: "TableTestMessage")
     var orderList = [CustomerOrder]()
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func viewDidLoad() {
         super.viewDidLoad()
         insertNewObject(self, index: 0)
@@ -31,7 +35,7 @@ class OrdersTableViewController: UITableViewController {
                 for object in pfobjects! {
                     let uOrder = object["OrderHeader"] as! String
                     let uDesc = object["OrderDescription"] as! String
-                    let uNum: Int = 1
+                    let uNum: Int = object["OrderNumber"] as! Int
                     self.orderList.append(CustomerOrder(name: uOrder, number: uNum, message: uDesc))
                     print(uOrder)
                     self.tableView.reloadData()
@@ -49,24 +53,44 @@ class OrdersTableViewController: UITableViewController {
     }
     
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
     // MARK: - Table view data source
     
+    /**********************************
+    *
+    *
+    **********************************/
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return orderList.count
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell: UITableViewCell
         
@@ -80,10 +104,18 @@ class OrdersTableViewController: UITableViewController {
         return cell
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         return false
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             orderList.removeAtIndex(indexPath.row)
@@ -93,6 +125,10 @@ class OrdersTableViewController: UITableViewController {
         }
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     func insertNewObject(sender: AnyObject, index: Int) {
         //   orderList.insert(, atIndex: 0)
         let indexPath = NSIndexPath(forRow: index, inSection: 0)
@@ -100,6 +136,10 @@ class OrdersTableViewController: UITableViewController {
     }
     
     
+    /**********************************
+     *
+     *
+     **********************************/
     //alert box method
     override func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
         
@@ -110,24 +150,39 @@ class OrdersTableViewController: UITableViewController {
     }
     
     
+    /**********************************
+     *
+     *
+     **********************************/
     // MARK: - Segues
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showOrderDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
                 
                 let object = orderList[indexPath.row]
-                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! OrdersDetailViewController
+                let controller = segue.destinationViewController as! OrdersDetailViewController
                 controller.detailItem = object
                 controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
                 controller.navigationItem.leftItemsSupplementBackButton = true
+                controller.orderHeader = object.orderName
+                controller.orderDescription = object.orderMessage
+                controller.orderNumber = object.orderNumber
             }
         }
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     @IBAction func AddNewRow(sender: UIButton) {
         insertNewObject(self, index: 1)
     }
     
+    /**********************************
+     *
+     *
+     **********************************/
     @IBAction func drawerMenuClicked(sender: UIBarButtonItem) {
         let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.centerContainer!.toggleDrawerSide(MMDrawerSide.Left, animated: true, completion: nil)
