894390f687b2bb0ccf932d78b23bb40376a6c72c
travkeel <keeltr@mail.gvsu.edu>
2015-11-02 14:50:05 -0500

    added accepted orders view.

--- Capstone/AcceptedOrdersTableViewController.swift
+++ Capstone/AcceptedOrdersTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  OrderRequestTableViewController.swift
+//  AcceptedOrdersTableViewController.swift
 //  Capstone
 //
 //  Created by Travis Keel on 11/2/15.
@@ -7,32 +7,13 @@
 //
 
 import UIKit
-import Parse
 
-class OrderRequestTableViewController: UITableViewController {
+class AcceptedOrdersTableViewController: UITableViewController {
+
+    var acceptedOrders = [CustomerOrder]()
     
-    var orderList = [CustomerOrder]()
-    var refreshController = UIRefreshControl()
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.refreshControl = refreshController
-        self.refreshControl?.addTarget(self, action: "didRefresh", forControlEvents: .ValueChanged)
-        
-        getOrders()
-
-    }
-    
-    /**********************************
-    *
-    *
-    **********************************/
-    func didRefresh() {
-        self.refreshControl?.beginRefreshing()
-        orderList.removeAll()
-        getOrders()
-        self.tableView.reloadData()
-        self.refreshControl?.endRefreshing()
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,7 +27,7 @@ class OrderRequestTableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return orderList.count
+        return acceptedOrders.count
     }
 
     /*
@@ -93,23 +74,8 @@ class OrderRequestTableViewController: UITableViewController {
         return true
     }
     */
-    
-    /**********************************
-    *
-    *
-    **********************************/
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: UITableViewCell
-        
-      
-        cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-        let object = orderList[indexPath.row]
-        cell.textLabel!.text = object.orderName
-        
-        return cell
-    }
 
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -117,45 +83,6 @@ class OrderRequestTableViewController: UITableViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-
-    
-    
-    /**********************************
-    *
-    *
-    **********************************/
-    func getOrders(){
-        let query = PFQuery(className: "Order")
-        query.whereKeyExists("OrderHeader")
-        
-        query.findObjectsInBackgroundWithBlock { (objects: [PFObject]?, error: NSError?) in
-            if error == nil {
-                let pfobjects = objects
-                if objects != nil {
-                    for object in pfobjects! {
-                        let uOrder = object["OrderHeader"] as! String
-                        let uDesc = object["OrderDescription"] as! String
-                        let uNum: Int = 1 //object["OrderNumber"] as! Int
-                        self.orderList.append(CustomerOrder(name: uOrder, number: uNum, message: uDesc))
-                        print(uOrder)
-                        self.tableView.reloadData()
-                    }
-                }
-                else {
-                    print("Error: \(error!)")
-                } }
-            
-        }
-    }
-    
-    /**********************************
-    *
-    *
-    **********************************/
-    func insertNewObject(sender: AnyObject, index: Int) {
-
-        _ = (forRow: index, inSection: 0)
-
-    }
+    */
 
 }
