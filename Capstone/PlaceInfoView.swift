b7888ce3f7dee1123d06db56dd49e3ef05028bdc
Ethan Christensen <Ethan@Ethans-MBP.hsd1.mi.comcast.net>
2015-11-06 18:19:59 -0500

    Added places popup view to home screen map

---
+++ Capstone/PlaceInfoView.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceInfoView.swift
+//  Capstone
+//
+//  Created by Ethan Christensen on 11/6/15.
+//  Copyright © 2015 John Tunisi. All rights reserved.
+//
+
+import UIKit
+
+class PlaceInfoView: UIView {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+
+}
