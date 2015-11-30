1ca0007d8924956792d7984b0de3b36b557c1e21
tunisij <tunisij@gmail.com>
2015-11-29 21:41:04 -0500

    Pushed assets

--- Capstone/AddNewDeliveryLocationViewController.swift
+++ Capstone/AddNewDeliveryLocationViewController.swift
@@ -8,6 +8,10 @@
 
 class AddNewDeliveryLocationViewController: UIViewController, UIPickerViewDelegate {
     
+    @IBOutlet weak var locationNickame: UITextField!
+    @IBOutlet weak var streetAddress: UITextField!
+    @IBOutlet weak var zipCode: UITextField!
+    @IBOutlet weak var city: UITextField!
     @IBOutlet weak var statePickerView: UIPickerView!
     
     let states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
